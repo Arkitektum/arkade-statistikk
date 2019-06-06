1c6edc8262427e656ac78d8aa74af1d724d650fc
Benjamin Dehli <benjamin@arkitektum.no>
2019-06-06 16:31:14 +0200

    Add frameworks and basic boilerplate

---
+++ src/pages/Start.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='content'></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  watch: {},
+  computed: {},
+  mounted() {},
+  methods: {}
+};
+</script>
+
+<style>
+
+</style>
